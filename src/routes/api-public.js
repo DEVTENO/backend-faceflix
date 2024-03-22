ea79a4b4f9928ee4d02c1a70ce350d4ace8eb7d7
rafi tanujaya <rafibussines@gmail.com>
2024-03-22 20:29:00 +0700

    feat: add feature login and get-current user

--- src/routes/api-public.js
+++ src/routes/api-public.js
@@ -4,3 +4,5 @@ import userController from "../controllers/user-controller";
 export const apiPublic = express.Router();
 
 apiPublic.post('/api/users/register', userController.register)
+apiPublic.post('/api/users/login', userController.login)
+
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,8 +1,14 @@
 import { ResponseError } from "../exception/response-error";
 import UserModel from "../models/UserModel";
-import userValidation from "../validation/user-validation"
+import userValidation from "../validation/user-validation";
 import { validation } from "../validation/validation"
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv';
+import ImageModel from "../models/ImageModel";
+import VideoModel from "../models/VideoModel";
+import BlogModel from "../models/BlogModel";
+dotenv.config();
 
 const register = async (request) => {
 
@@ -23,6 +29,64 @@ const register = async (request) => {
     return user;
 }
 
+const login = async (request) => {
+    const userRequest = await validation(userValidation.login, request);
+    const user = await UserModel.findOne({email: userRequest.email})
+    if(!user) {
+        throw new ResponseError(400, 'username or password is wrong')
+    }
+
+    const passwordIsValid = await bcrypt.compare(userRequest.password, user.password);
+
+    if(passwordIsValid) {
+        const payload = {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            title: user.title,
+            profileImage: user.profileImage,
+            backgroundImage: user.backgroundImage,
+            description: user.description
+        }
+        const expire = 60 * 60 * 1;
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: expire})
+
+        return {
+            user: payload,
+            token: token
+        }
+    } else {
+        throw new ResponseError(400, 'username or password is wrong')
+    }
+}
+
+const get = async (email) => {
+    const emailRequest = await validation(userValidation.get, email);
+
+    const user = await UserModel.findOne({email: emailRequest});
+    if(!user) {
+        throw new ResponseError(404, 'user not found')
+    }
+    const postImage = await ImageModel.find({userId: user.id});
+    const postVideo = await VideoModel.find({userId: user.id});
+    const postBlog =  await BlogModel.find({userId: user.id});
+
+    return {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            title: user.title,
+            profileImage: user.profileImage,
+            backgroundImage: user.backgroundImage,
+            description: user.description,
+            countImage: postImage.length,
+            countVideo: postVideo.length,
+            countBlog: postBlog.length
+    }
+}
+
 export default {
-    register
+    register,
+    login,
+    get
 }
