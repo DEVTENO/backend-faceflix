07a6fcc99ed4435cbad0128066bee43ff80b5a96
rafi tanujaya <rafibussines@gmail.com>
2024-03-22 20:22:55 +0700

    feat: add feature handle respon error

--- src/middlewares/error-middleware.js
+++ src/middlewares/error-middleware.js
@@ -1,14 +1,17 @@
 import { ResponseError } from "../exception/response-error";
 
 export const errorMiddleware = async (err, req, res, next) => {
-    console.log(err);
-    if(!err){
-        return next()
-    }
+  if (!err) {
+    return next();
+  }
 
-    if(err instanceof ResponseError){
-        res.status(400).json({
-            errors: err.message
-        })
-    }
-}
+  if (err instanceof ResponseError) {
+    res.status(400).json({
+      errors: err.message,
+    });
+  } else {
+    res.status(500).json({
+      errors: err.message,
+    });
+  }
+};
