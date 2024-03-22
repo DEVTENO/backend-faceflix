c87992dbeebaad6fff1593cc9bf9c07fc1982cd3
rafi tanujaya <rafibussines@gmail.com>
2024-03-22 20:20:40 +0700

    feat: add feature auth middleware jwt

---
+++ src/middlewares/auth-middleware.js
@@ -0,0 +1,28 @@
+import { ResponseError } from "../exception/response-error";
+import jwt from 'jsonwebtoken'
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+export const authMiddleware = async (req, res, next) => {
+    const {authorization} = req.headers;
+
+    if(!authorization) {
+        res.status(401).json({
+            statusCode: 401,
+            errors: 'Unauthorized'
+        }).end()
+    }
+
+    const token = authorization.split(' ')[1];
+    try {
+        const jwtDecode = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = jwtDecode;
+    } catch (error) {
+        res.status(401).json({
+            statusCode: 401,
+            errors: 'Unauthorized'
+        }).end()
+    }
+    next()
+
+}
