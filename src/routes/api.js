181640f5a304a35e4ae6ceb9da3a7c45ca01a3d8
rafi tanujaya <rafibussines@gmail.com>
2024-03-25 00:13:06 +0700

    feat: add feature update profile

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,18 @@
-import express from "express"
+import express from "express";
 import userController from "../controllers/user-controller.js";
 import { authMiddleware } from "../middlewares/auth-middleware.js";
+import { userUpload } from "../application/multer.js";
 
 export const api = express.Router();
 
-api.use(authMiddleware)
+api.use(authMiddleware);
 
-api.get('/api/users/current', userController.get)
+api.get("/api/users/current", userController.get);
+api.patch(
+  "/api/users/current/profile",
+  userUpload.fields([
+    { name: "profileImage", maxCount: 1 },
+    { name: "backgroundImage", maxCount: 1 },
+  ]),
+  userController.update
+);
